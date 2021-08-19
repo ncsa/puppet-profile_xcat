11dac592940961705a46951ebedcf5e8b7b1b57b
Andy Loftus <aloftus@gmail.com>
2021-08-19 18:39:22 +0000

    ASDPLNG-49 dont run client setup on xcat server

--- manifests/client.pp
+++ manifests/client.pp
@@ -5,5 +5,12 @@
 # @example
 #   include profile_xcat::client
 class profile_xcat::client {
-  include ::profile_xcat::client::ssh
+
+  $master_node_ip = lookup( 'profile_xcat::master_node_ip' )
+  $my_ip = $facts['networking']['ip']
+
+  # OK to run if this is not the xcat server
+  if $master_node_ip != $my_ip {
+    include ::profile_xcat::client::ssh
+  }
 }
