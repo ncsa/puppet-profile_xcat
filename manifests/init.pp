2429878a60c9e9bd28588a979506a6900471f85b
David King <kingda@illinois.edu>
2021-10-20 12:42:55 -0500

    Add the ability to have defined bind IP when the mgmt network is routed

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,6 +14,7 @@ class profile_xcat (
   Array $ipmi_net_cidrs,
   Array $mgmt_net_cidrs,
   String $master_node_ip,
+  String $ipmi_bind_ip,
 ) {
   # nothing to do here, just define class parameters above for use in the
   # other classes
